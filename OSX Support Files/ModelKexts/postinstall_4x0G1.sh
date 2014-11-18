f94f3079bd5ca38da232e1c4d8ab1e245569453c
Dinh Hai Nguyen <dhnguyenit@gmail.com>
2014-11-18 11:53:40 +0700

    Add fan patches for haswell laptops

--- OSX Support Files/ModelKexts/postinstall_4x0G1.sh
+++ OSX Support Files/ModelKexts/postinstall_4x0G1.sh
@@ -1,16 +1,6 @@
 #!/bin/bash
 
 
-if [ -d /Volumes/EFI/EFI/CLOVER/kexts/10.9/ACPIPoller.kext ]
-then
-	rm -rf /Volumes/EFI/EFI/CLOVER/kexts/10.9/ACPIPoller.kext
-fi
-
-if [ -d /Volumes/EFI/EFI/CLOVER/kexts/10.10/ACPIPoller.kext ]
-then
-	rm -rf /Volumes/EFI/EFI/CLOVER/kexts/10.10/ACPIPoller.kext
-fi
-
 if [ -d /Volumes/EFI/EFI/CLOVER/kexts/10.9/AppleIntelE1000e.kext ]
 then
 	rm -rf /Volumes/EFI/EFI/CLOVER/kexts/10.9/AppleIntelE1000e.kext
--- OSX Support Files/ModelKexts/postinstall_8x0G1.sh
+++ OSX Support Files/ModelKexts/postinstall_8x0G1.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
 
-
-if [ -d /Volumes/EFI/EFI/CLOVER/kexts/10.9/ACPIPoller.kext ]
-then
-	rm -rf /Volumes/EFI/EFI/CLOVER/kexts/10.9/ACPIPoller.kext
-fi
-
-if [ -d /Volumes/EFI/EFI/CLOVER/kexts/10.10/ACPIPoller.kext ]
-then
-	rm -rf /Volumes/EFI/EFI/CLOVER/kexts/10.10/ACPIPoller.kext
-fi
-
 if [ -d /Volumes/EFI/EFI/CLOVER/kexts/10.9/RealtekRTL8111.kext ]
 then
 	rm -rf /Volumes/EFI/EFI/CLOVER/kexts/10.9/RealtekRTL8111.kext
