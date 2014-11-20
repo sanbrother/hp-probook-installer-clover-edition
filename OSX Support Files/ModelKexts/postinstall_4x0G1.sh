d8348fb332974e737a25759a397ca33e4eaa4d9a
Dinh Hai Nguyen <dhnguyenit@gmail.com>
2014-11-20 22:04:14 +0700

    Revert "Switch from GenericUSBXHCI in haswell laptops"
    This reverts commit ab47ed5af2eb6eeb5c72fb9c947a345e2336ad2f.

--- OSX Support Files/ModelKexts/postinstall_4x0G1.sh
+++ OSX Support Files/ModelKexts/postinstall_4x0G1.sh
@@ -10,13 +10,3 @@ if [ -d /Volumes/EFI/EFI/CLOVER/kexts/10.10/AppleIntelE1000e.kext ]
 then
 	rm -rf /Volumes/EFI/EFI/CLOVER/kexts/10.10/AppleIntelE1000e.kext
 fi
-
-if [ -d /Volumes/EFI/EFI/CLOVER/kexts/10.9/GenericUSBXHCI.kext ]
-then
-	rm -rf /Volumes/EFI/EFI/CLOVER/kexts/10.9/GenericUSBXHCI.kext
-fi
-
-if [ -d /Volumes/EFI/EFI/CLOVER/kexts/10.10/GenericUSBXHCI.kext ]
-then
-	rm -rf /Volumes/EFI/EFI/CLOVER/kexts/10.10/GenericUSBXHCI.kext
-fi
--- OSX Support Files/ModelKexts/postinstall_8x0G1.sh
+++ OSX Support Files/ModelKexts/postinstall_8x0G1.sh
@@ -9,13 +9,3 @@ if [ -d /Volumes/EFI/EFI/CLOVER/kexts/10.10/RealtekRTL8111.kext ]
 then
 	rm -rf /Volumes/EFI/EFI/CLOVER/kexts/10.10/RealtekRTL8111.kext
 fi
-
-if [ -d /Volumes/EFI/EFI/CLOVER/kexts/10.9/GenericUSBXHCI.kext ]
-then
-	rm -rf /Volumes/EFI/EFI/CLOVER/kexts/10.9/GenericUSBXHCI.kext
-fi
-
-if [ -d /Volumes/EFI/EFI/CLOVER/kexts/10.10/GenericUSBXHCI.kext ]
-then
-	rm -rf /Volumes/EFI/EFI/CLOVER/kexts/10.10/GenericUSBXHCI.kext
-fi
